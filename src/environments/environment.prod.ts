d4cf2af1d50ee4790c81e8168d0a92772b195219
shajmil-ufs <shajmil@ufstechnologies.com>
2024-03-05 14:48:53 +0530

    new prod link included

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,9 +3,11 @@ export const environment = {
 
 
 
-	BasePath: 'http://18.236.152.62:8080/',
-        FilePath:'https://ufsnabeelphotoalbum.s3.us-east-2.amazonaws.com/',
-        NotificationPath:'https://darlsconotificationapi.ufstech.co.in/',
-
+	BasePath: 'https://chemmannoorapi.ufstech.in/',
+    NotificationPath:'https://chemmannoornotificationapi.ufstech.in/',
+    FilePath:'https://ufsnabeelphotoalbum.s3.us-east-2.amazonaws.com/',
+        
+       
+        
 
 }
